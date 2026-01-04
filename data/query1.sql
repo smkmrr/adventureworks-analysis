182e08c7d1053735c059043c24643ac13a9a1537
Ali Akyel <aakyel@gmail.com>
2026-01-04 13:31:33 +0300

    feat: Polish the format and add insights until 4th question.

--- data/query1.sql
+++ data/query1.sql
@@ -3,7 +3,7 @@ SELECT
     pc.Name AS CategoryName,
     COUNT(p.ProductID) AS ProductCount
 FROM Production.ProductCategory AS pc
-JOIN Production.ProductSubcategory AS ps 
+INNER JOIN Production.ProductSubcategory AS ps 
     ON pc.ProductCategoryID = ps.ProductCategoryID
 JOIN Production.Product AS p 
     ON ps.ProductSubcategoryID = p.ProductSubcategoryID
--- data/query3.sql
+++ data/query3.sql
@@ -1,7 +1,8 @@
-/* query3.sql */
+/* query3.sql - Fixed Column Name */
 SELECT 
-    FORMAT(OrderDate, 'yyyy-MM') AS OrderMonth,
+    -- We force the name 'OrderDate' here so Python can find it
+    DATEFROMPARTS(YEAR(OrderDate), MONTH(OrderDate), 1) AS OrderDate, 
     SUM(SubTotal) AS TotalSales
 FROM Sales.SalesOrderHeader
-GROUP BY FORMAT(OrderDate, 'yyyy-MM')
-ORDER BY OrderMonth;
+GROUP BY YEAR(OrderDate), MONTH(OrderDate)
+ORDER BY OrderDate;
